c5fd303910a11d8d4d788776d1d42c2cddb97a96
kayutenko <kayutenko@mail.ru>
2017-02-19 19:47:30 +0300

    delete old

--- angular-quickstart/app/app.component.js
+++
@@ -1,9 +0,0 @@
-(function(app){
-	app.MyComponent = 
-		ng.core.Component({
-			selector: 'hello-world',
-			template: '<h2>Fuck this world</h2>'
-		}).
-		Class({constructor: function () {}
-	});
-})(window.app || (window.app = {}));
--- angular-quickstart/app/main.js
+++
@@ -1,5 +0,0 @@
-(function(app) {
-	document.addEventListener('DOMContentLoaded', function () {
-		ng.platformBrowserDynamic.platformBrowserDynamic().bootstrapModule(app.MyModuleApp);
-	});
-})(window.app || (window.app = {}));
